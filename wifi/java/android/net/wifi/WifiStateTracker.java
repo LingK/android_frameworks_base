ca20bed7dbd3838902d9f2c182f0e188aafddfb2
Liquid <rjbirnesser@gmail.com>
2011-08-21 23:03:15 -0400

    Revert Bad WiFi Changes

--- wifi/java/android/net/wifi/WifiStateTracker.java
+++ wifi/java/android/net/wifi/WifiStateTracker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2011 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -168,9 +168,6 @@ public class WifiStateTracker extends NetworkStateTracker {
      * value if a Settings value is not present.
      */
     private static final int DEFAULT_MAX_DHCP_RETRIES = 9;
-
-    //Minimum dhcp lease duration for renewal
-    private static final int MIN_RENEWAL_TIME_SECS = 5 * 60; //5 minutes
 
     private static final int DRIVER_POWER_MODE_AUTO = 0;
     private static final int DRIVER_POWER_MODE_ACTIVE = 1;
@@ -2576,11 +2573,8 @@ public class WifiStateTracker extends NetworkStateTracker {
                     if (msg.what == EVENT_DHCP_START) {
                         if (runDhcp(false)) {
                             event = EVENT_INTERFACE_CONFIGURATION_SUCCEEDED;
-                            Log.d(TAG, "DHCP succeeded with lease: " + mDhcpInfo.leaseDuration);
-                            setDhcpRenewalAlarm(mDhcpInfo.leaseDuration);
                        } else {
                             event = EVENT_INTERFACE_CONFIGURATION_FAILED;
-                            Log.e(TAG, "DHCP request failed: " + NetworkUtils.getDhcpError());
                         }
                         synchronized (this) {
                             if (!mCancelCallback) {
@@ -2612,8 +2606,6 @@ public class WifiStateTracker extends NetworkStateTracker {
                                         mNetworkInfo);
                                 msg.sendToTarget();
                             }
-
-                            setDhcpRenewalAlarm(mDhcpInfo.leaseDuration);
                         } else {
                             event = EVENT_INTERFACE_CONFIGURATION_FAILED;
                             Log.d(TAG, "DHCP renewal failed: " + NetworkUtils.getDhcpError());
@@ -2689,20 +2681,6 @@ public class WifiStateTracker extends NetworkStateTracker {
             int state = mBluetoothHeadset.getState(mBluetoothHeadset.getCurrentHeadset());
             return state == BluetoothHeadset.STATE_DISCONNECTED;
         }
-
-        private void setDhcpRenewalAlarm(long leaseDuration) {
-            //Do it a bit earlier than half the lease duration time
-            //to beat the native DHCP client and avoid extra packets
-            //48% for one hour lease time = 29 minutes
-            if (leaseDuration < MIN_RENEWAL_TIME_SECS) {
-                leaseDuration = MIN_RENEWAL_TIME_SECS;
-            }
-            mAlarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                    SystemClock.elapsedRealtime() +
-                    leaseDuration * 480, //in milliseconds
-                    mDhcpRenewalIntent);
-        }
-
     }
     
     private void checkUseStaticIp() {
