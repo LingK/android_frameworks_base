f5c5288a4b65e2de950d8d488e968de0bbf31580
Liquid <rjbirnesser@gmail.com>
2011-08-30 22:41:53 -0400

    MIUI-like Battery Bar (1/2)
    Implement a MIUI-like battery bar.
    Battery level is shown as a bar running across the top of the status bar.
    
    Adapted from OMFGB: github.com/OMFGB/frameworks_base/commit/996234
    
    Change-Id: I99830a5559a6c27d21c655f030610954d9af935a
    
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/StatusBarPolicy.java

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarPolicy.java
@@ -1665,8 +1665,8 @@ public class StatusBarPolicy {
         ContentResolver resolver = mContext.getContentResolver();
 
         mShowCmBattery = (Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_CM_BATTERY, 0) == 1);
-        mCmBatteryStatus = !mShowCmBattery;     
+                Settings.System.STATUS_BAR_CM_BATTERY, 0) > 0);
+        mCmBatteryStatus = !mShowCmBattery;
         mService.setIconVisibility("battery", !mShowCmBattery);
 
 		mShowPhoneSignal = (Settings.System.getInt(resolver,
