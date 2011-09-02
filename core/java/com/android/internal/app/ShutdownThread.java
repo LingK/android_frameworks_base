17d6774e5fd9be33e60afce60b5d4bfdfd9e4791
Liquid <rjbirnesser@gmail.com>
2011-09-01 21:33:34 -0400

    Attempt Quick Restart

--- core/java/com/android/internal/app/ShutdownThread.java
+++ core/java/com/android/internal/app/ShutdownThread.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2011 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -112,8 +112,12 @@ public final class ShutdownThread extends Thread {
                                 if (which < 0)
                                     return;
 
-                                String actions[] = context.getResources().getStringArray(com.android.internal.R.array.shutdown_reboot_actions);
-
+                                String actions[] = context.getResources().getStringArray
+                                    (com.android.internal.R.array.shutdown_reboot_actions);
+                                                      
+                                if (actions == "hotreboot" && which < actions.length)
+                                    rebootOrShutdown(true, "pkill sys");
+                    
                                 if (actions != null && which < actions.length)
                                     mRebootReason = actions[which];
                             }
