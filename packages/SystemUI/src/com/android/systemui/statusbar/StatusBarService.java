8e821b8282270b861658f375bce732ae66e41d3b
Liquid <rjbirnesser@gmail.com>
2011-09-07 21:50:20 -0400

    Fix Broken Build

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarService.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarService.java
@@ -21,11 +21,11 @@ import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.statusbar.StatusBarIconList;
 import com.android.internal.statusbar.StatusBarNotification;
 import com.android.systemui.R;
-import com.android.systemui.statusbar.StatusBarView.SettingsObserver;
 import com.android.systemui.statusbar.powerwidget.PowerWidget;
 import com.android.systemui.statusbar.powerwidget.PowerWidgetBottom;
 
 import android.app.ActivityManagerNative;
+import android.app.AlarmManager;
 import android.app.Dialog;
 import android.app.Notification;
 import android.app.PendingIntent;
@@ -705,7 +705,6 @@ public class StatusBarService extends Service implements CommandQueue.Callbacks 
 
         mPowerWidget.setupWidget();
         mPowerWidgetBottom.setupWidget();
-	    mMusicControls.setupControls();
     }
 
     public void addIcon(String slot, int index, int viewIndex, StatusBarIcon icon) {
