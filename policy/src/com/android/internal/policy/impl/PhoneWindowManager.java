a61b6517ecf96aecaea0d12e0985f0a1930d2e7d
Liquid <rjbirnesser@gmail.com>
2011-09-08 15:36:12 -0400

    Implement streamlined menu button behavior.
    Pressing the home button shows the recent applications list immediately.
    This allows switching between recent apps instantly, rather than waiting
    for a long press to occur, giving a much more streamlined way to jump
    between apps.
    
    The behavior when pressing and releasing and when long-pressing is
    "user-habits" compatible with the standard behavior.  Pressing and
    releasing quickly still goes to the home menu; long-pressing still pins
    the recent menu open.
    
    I think this could be the default, but this is a major UI, so I've
    left the standard behavior as the default pending feedback.
    
    Change-Id: Id5253c258fe2fed26fb23f5288b27a4d5f5c27cd
    Signed-off-by: Glenn Maynard <glenn@zewt.org>
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -551,6 +551,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 mContext.getContentResolver(), Settings.Secure.DEVICE_PROVISIONED, 0) != 0;
     }
 
+    private boolean useQuickHomeKey() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.USE_QUICK_RECENT, 0) != 0;
+    }
     /**
      * When a home-key longpress expires, close other system windows and launch the recent apps
      */
@@ -562,6 +566,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
              */
             mHomePressed = false;
             performHapticFeedbackLw(null, HapticFeedbackConstants.LONG_PRESS, false);
+
+            if (useQuickHomeKey()) {
+                /* The recent apps dialog has already been displayed, and all we're doing on long
+                 * press is the above, to pin the recent dialog on screen. */
+                return;
+            }
+
             if (Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.USE_CUSTOM_APP, 0) != 0) {
                 runCustomApp();
@@ -572,16 +583,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     };
 
-    /**
-     * Short press also has an expire time to destinguish double tap from single tap.
-     */
-    Runnable mHomeShortPress = new Runnable() {
+    Runnable mDisplayRecent = new Runnable() {
         public void run() {
-            launchHomeFromHotKey();
+            sendCloseSystemWindows(SYSTEM_DIALOG_REASON_RECENT_APPS);
+            showRecentAppsDialog();
         }
     };
 
-	Runnable mBackLongPress = new Runnable() {
+    Runnable mBackLongPress = new Runnable() {
         public void run() {
             if (Settings.Secure.getInt(mContext.getContentResolver(),
                 Settings.Secure.KILL_APP_LONGPRESS_BACK, 0) == 0) {
@@ -677,6 +686,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mRecentAppsDialog = new RecentApplicationsDialog(mContext);
         }
         mRecentAppsDialog.show();
+    }
+
+    /**
+     * Hide the recent apps dialog, if shown
+     */
+    void hideRecentAppsDialog() {
+        if (mRecentAppsDialog == null)
+            return;
+        mRecentAppsDialog.cancel();
     }
 
     void runCustomApp() {
@@ -1395,9 +1413,22 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             
             if (down && repeatCount == 0) {
                 if (!keyguardOn) {
+                    if (useQuickHomeKey()) {
+                        /* Show the recent apps dialog immediately.  We can't do that from here; post a message
+                         * to handle it. */
+                        mHandler.post(mDisplayRecent);
+                    }
                     mHandler.postDelayed(mHomeLongPress, ViewConfiguration.getGlobalActionKeyTimeout());
                 }
                 mHomePressed = true;
+            } else if (!down && useQuickHomeKey()) {
+                if (mRecentAppsDialog != null) {
+                    if(mRecentAppsDialog.isShowing() && mHomePressed) {
+                        /* If the apps dialog is still shown, then the home button was released without
+                         * selecting anything.  If the dialog wasn't pinned by mHomeLongPress, hide it. */
+                        hideRecentAppsDialog();
+                    }
+                }
             }
             return true;
         } else if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -1538,6 +1569,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
      * given the situation with the keyguard.
      */
     void launchHomeFromHotKey() {
+        if (useQuickHomeKey()) {
+            // Make sure we always hide the recent dialog when we're going home.
+            hideRecentAppsDialog();
+        }
+
         if (mKeyguardMediator.isShowingAndNotHidden()) {
             // don't launch home if keyguard showing
         } else if (!mHideLockScreen && mKeyguardMediator.isInputRestricted()) {
