5934c2bd578dd96f30b115e92b7b19731a216322
Liquid <rjbirnesser@gmail.com>
2011-09-08 20:57:24 -0400

    Fix Double Tap Home

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -590,6 +590,15 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     };
 
+    /**
+     * Short press also has an expire time to destinguish double tap from single tap.
+     */
+    Runnable mHomeShortPress = new Runnable() {
+        public void run() {
+            launchHomeFromHotKey();
+        }
+    };
+
     Runnable mBackLongPress = new Runnable() {
         public void run() {
             if (Settings.Secure.getInt(mContext.getContentResolver(),
