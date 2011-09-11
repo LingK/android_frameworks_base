8ebd2b298a59a50fe73cc470f3feec850560aaea
Liquid <rjbirnesser@gmail.com>
2011-09-10 20:03:23 -0400

    Fix can't change in-call volume with screen off
    This is a fix for the following issue:
    http://code.google.com/p/cyanogenmod/issues/detail?id=4070
    
    Issue: Proximity sensor disables screen wake using power button. With the
    "volume rocker wake" option enabled in CMParts, volume keys emulate the power
    button whenever the screen is off. If the screen is off due to proximity
    sensor, volume keys become useless.
    
    Solution: When screen is off due to the proximity sensor, do NOT override the
    volume keys as power buttons. This allows the user to adjust in-call volume
    using volume keys while phone is to their ear.
    
    Also: Small clean-up of unecessary code and style fixes.
    
    Change-Id: I9f21bc1a59f940242b2196d423a7b8de62c855e0
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -238,6 +238,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     int mLidKeyboardAccessibility;
     int mLidNavigationAccessibility;
     boolean mScreenOn = false;
+    int mScreenOffReason;
     boolean mOrientationSensorEnabled = false;
     int mCurrentAppOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
     static final int DEFAULT_ACCELEROMETER_ROTATION = 0;
@@ -2125,14 +2126,25 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             // to wake the device but don't pass the key to the application.
             result = 0;
 
-            final boolean isWakeKey = (policyFlags
-		    & (WindowManagerPolicy.FLAG_WAKE | WindowManagerPolicy.FLAG_WAKE_DROPPED)) != 0
-		    || (keyCode == BTN_MOUSE) && mTrackballWakeScreen
-		    || (keyCode == KeyEvent.KEYCODE_VOLUME_UP) && mVolumeWakeScreen
-		    || (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) && mVolumeWakeScreen;
+            boolean isWakeKey = (policyFlags
+                    & (WindowManagerPolicy.FLAG_WAKE | WindowManagerPolicy.FLAG_WAKE_DROPPED)) != 0
+                    || ((keyCode == BTN_MOUSE) && mTrackballWakeScreen)
+                    || ((keyCode == KeyEvent.KEYCODE_VOLUME_UP) && mVolumeWakeScreen)
+                    || ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) && mVolumeWakeScreen);
+
+            // Don't wake the screen if we have not set the option "wake with volume" in CMParts
+            // OR if "wake with volume" is set but screen is off due to proximity sensor
+            // regardless if WAKE Flag is set in keylayout
+            final boolean isOffByProx = (mScreenOffReason == WindowManagerPolicy.OFF_BECAUSE_OF_PROX_SENSOR);
+
+            if (isWakeKey
+                    && (!mVolumeWakeScreen || isOffByProx)
+                    && ((keyCode == KeyEvent.KEYCODE_VOLUME_UP) || (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN))) {
+                isWakeKey = false;
+            }
 
             // make sure keyevent get's handled as power key on volume-wake
-            if(!isScreenOn && mVolumeWakeScreen && isWakeKey && ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+            if(mVolumeWakeScreen && isWakeKey && ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)
                     || (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)))
             	keyCode=KeyEvent.KEYCODE_POWER;
 
@@ -2406,6 +2418,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mKeyguardMediator.onScreenTurnedOff(why);
         synchronized (mLock) {
             mScreenOn = false;
+            mScreenOffReason = why;
             updateOrientationListenerLp();
             updateLockScreenTimeout();
         }
