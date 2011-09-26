87597ec82debd05712e85a477abc3843c72ffabf
Liquid <rjbirnesser@gmail.com>
2011-09-26 16:01:32 -0400

    Add New Battery Bar & With Color Support

--- packages/SystemUI/src/com/android/systemui/statusbar/StatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/StatusBarView.java
@@ -16,120 +16,62 @@
 
 package com.android.systemui.statusbar;
 
-import android.content.BroadcastReceiver;
-import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.ContentObserver;
-import android.os.Handler;
-import android.provider.Settings;
+import android.content.res.Configuration;
+import android.graphics.Canvas;
+import android.os.SystemClock;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 
+import android.content.Intent;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.view.IWindowManager;
+import android.view.KeyEvent;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.util.Slog;
 import com.android.systemui.R;
 
 public class StatusBarView extends FrameLayout {
 
     private static final String TAG = "StatusBarView";
     static final int DIM_ANIM_TIME = 400;
-
-    Handler mHandler;
+    
+    View mDate;
     boolean mTracking;
     int mStartX, mStartY;
-    boolean mScreenOn = true;
-    StatusBarService mService;
-    FixedSizeDrawable mBackground;
-    private boolean mAttached = false;
-
-    View mDate;
-    ViewGroup mNotificationIcons;
     ViewGroup mStatusIcons;
-    View mBatteryIndicator;
-    View mBatteryChargingIndicator;
-
-    class SettingsObserver extends ContentObserver {
-        public SettingsObserver(Handler handler) {
-            super(handler);
-        }
-
-        void observer() {
-            ContentResolver resolver = mContext.getContentResolver();
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.STATUS_BAR_BATTERY), false, this);
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.COLOR_AUTO_COLOR), false, this);
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.COLOR_AUTO_CHARGING), false, this);
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.COLOR_AUTO_LOW), false, this);
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.COLOR_AUTO_MEDIUM), false, this);
-            resolver.registerContentObserver(
-                    Settings.System.getUriFor(Settings.System.COLOR_STATIC_VALUE), false, this);
-        }
-
-        @Override
-        public void onChange(boolean selfChange) {
-            updateSettings();
-        }
-    }
+    StatusBarService mService;
+    ViewGroup mNotificationIcons;
+    FixedSizeDrawable mBackground;
 
     public StatusBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        mHandler = new Handler();
-        SettingsObserver observer = new SettingsObserver(mHandler);
-        observer.observer();
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-
         mNotificationIcons = (ViewGroup)findViewById(R.id.notificationIcons);
         mStatusIcons = (ViewGroup)findViewById(R.id.statusIcons);
         mDate = findViewById(R.id.date);
+
         mBackground = new FixedSizeDrawable(mDate.getBackground());
         mBackground.setFixedBounds(0, 0, 0, 0);
         mDate.setBackgroundDrawable(mBackground);
-        mBatteryIndicator = findViewById(R.id.battery_indicator);
-        mBatteryChargingIndicator = findViewById(R.id.battery_indicator_charging);
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
+
         mService.onBarViewAttached();
-
-        if (!mAttached) {
-            mAttached = true;
-            IntentFilter filter = new IntentFilter();
-            filter.addAction(Intent.ACTION_SCREEN_OFF);
-            filter.addAction(Intent.ACTION_SCREEN_ON);
-            getContext().registerReceiver(mIntentReceiver, filter, null, getHandler());
-        }
     }
-
-    private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-
-            if (Intent.ACTION_SCREEN_OFF.equals(action)) {
-                mScreenOn = false;
-            } else if (Intent.ACTION_SCREEN_ON.equals(action)) {
-                mScreenOn = true;
-            }
-        }
-    };
 
     @Override
     protected void onDetachedFromWindow() {
@@ -151,6 +93,7 @@ public class StatusBarView extends FrameLayout {
 
         int oldDateRight = mDate.getRight();
         int newDateRight;
+
         newDateRight = getDateSize(mNotificationIcons, oldDateRight,
                 getViewOffset(mNotificationIcons));
 
@@ -165,7 +108,6 @@ public class StatusBarView extends FrameLayout {
                 }
             }
         }
-
         int max = r - getPaddingRight();
         if (newDateRight > max) {
             newDateRight = max;
@@ -173,46 +115,10 @@ public class StatusBarView extends FrameLayout {
 
         mDate.layout(mDate.getLeft(), mDate.getTop(), newDateRight, mDate.getBottom());
         mBackground.setFixedBounds(-mDate.getLeft(), -mDate.getTop(), (r-l), (b-t));
-        boolean batteryBar = (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_BATTERY, 0) == 2);
-
-        if (batteryBar) {
-            mBatteryIndicator.setVisibility(VISIBLE);
-            Intent batteryIntent = mContext.getApplicationContext().registerReceiver(null,
-                    new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-            int level = batteryIntent.getIntExtra("level", 0);
-            boolean plugged = batteryIntent.getIntExtra("plugged", 0) != 0;
-
-            updateColor(plugged, level);
-            mBatteryIndicator.layout(mBatteryIndicator.getLeft(), mBatteryIndicator.getTop(),
-                    ((r-l) * level) / 100, 2);
-
-            if (plugged) {
-                mBatteryChargingIndicator.setVisibility(VISIBLE);
-                mBatteryChargingIndicator.setBackgroundColor(0xFF33CC33);
-                int chargingWidth = Math.min(5, ((r-l) * (100 - level)) / 2);
-                mBatteryChargingIndicator.layout(r, t, (r + chargingWidth), t + 2);
-                Animation a = new TranslateAnimation(0, (float) level - (r-l), 0, 0);
-                a.setInterpolator(new AccelerateInterpolator());
-                a.setDuration(2000);
-                a.setRepeatCount(-1);
-                a.setRepeatMode(1);
-
-                if (mScreenOn) {
-                    mBatteryChargingIndicator.startAnimation(a);
-                } else {
-                    mBatteryChargingIndicator.clearAnimation();
-                }
-            }
-        } else {
-            mBatteryIndicator.setVisibility(GONE);
-            mBatteryChargingIndicator.setVisibility(GONE);
-        }
     }
 
     private int getViewOffset(View v) {
         int offset = 0;
-
         while (v != this) {
             offset += v.getLeft();
             ViewParent p = v.getParent();
@@ -228,7 +134,6 @@ public class StatusBarView extends FrameLayout {
 
     private int getDateSize(ViewGroup g, int w, int offset) {
         final int N = g.getChildCount();
-
         for (int i=0; i<N; i++) {
             View v = g.getChildAt(i);
             int l = v.getLeft() + offset;
@@ -253,6 +158,7 @@ public class StatusBarView extends FrameLayout {
     public boolean onInterceptTouchEvent(MotionEvent event, boolean skipService){
         if(skipService)
             return super.onInterceptTouchEvent(event);
+
         return mService.interceptTouchEvent(event)
                     ? true : super.onInterceptTouchEvent(event);
     }
@@ -260,59 +166,5 @@ public class StatusBarView extends FrameLayout {
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
         return onInterceptTouchEvent(event, false);
-    }
-
-    private void updateColor(boolean plugged, int batt) {
-        boolean autoColorBatteryText = Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.COLOR_AUTO_COLOR, 1) == 1 ? true : false;
-
-	    int color_static = Settings.System.getInt(getContext().getContentResolver(),
-		        Settings.System.COLOR_STATIC_VALUE, 0xFF99AC06);
-
-        int color_auto_charging = Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.COLOR_AUTO_CHARGING, 0xFF93D500);
-
-        int color_auto_medium = Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.COLOR_AUTO_MEDIUM, 0xFFD5A300);
-
-        int color_auto_low = Settings.System.getInt(getContext().getContentResolver(),
-                Settings.System.COLOR_AUTO_LOW, 0xFFD54B00);
-
-        if (autoColorBatteryText) {
-            if (plugged) {
-                mBatteryIndicator.setBackgroundColor(color_auto_charging);
-                mBatteryChargingIndicator.setBackgroundColor(color_auto_charging);
-            } else {
-                if (batt < 15) {
-                    mBatteryIndicator.setBackgroundColor(color_auto_low);
-                } else if (batt < 40) {
-                    mBatteryIndicator.setBackgroundColor(color_auto_medium);
-                } else {
-                    mBatteryIndicator.setBackgroundColor(color_static);
-                }
-            }
-        } else {
-            mBatteryIndicator.setBackgroundColor(color_static);
-            mBatteryChargingIndicator.setBackgroundColor(color_static);
-        }
-    }
-
-    private void updateSettings() {
-        boolean batteryBar = (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.STATUS_BAR_BATTERY, 0) == 2);
-
-        if (batteryBar) {
-            Intent batteryIntent = mContext.getApplicationContext().registerReceiver(null,
-                    new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-            boolean plugged = batteryIntent.getIntExtra("plugged", 0) != 0;
-            updateColor(plugged, batteryIntent.getIntExtra("level", 0));
-            mBatteryIndicator.setVisibility(VISIBLE);
-
-            if (plugged)
-                mBatteryChargingIndicator.setVisibility(VISIBLE);
-        } else {
-            mBatteryIndicator.setVisibility(GONE);
-            mBatteryChargingIndicator.setVisibility(GONE);
-        }
     }
 }
