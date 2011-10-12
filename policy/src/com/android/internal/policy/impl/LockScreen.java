b2200d858fd8cfb10ad45d888993a455ef0a7481
Danesh Mondegarian <daneshm90@gmail.com>
2011-10-12 16:54:11 -0400

    Lockscreen disabled unlock on keyboard slide + gesture.
    If gesture active, don't unlock screen upon sliding keyboard out.
    
    Change-Id: I08a1dd596cec5808cb1e62ad0dfa40f946199dd2
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/LockScreen.java

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -1339,7 +1339,8 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
             mKeyboardHidden = newConfig.hardKeyboardHidden;
             final boolean isKeyboardOpen = mKeyboardHidden == Configuration.HARDKEYBOARDHIDDEN_NO;
 
-            if (mSliderUnlockScreen && isKeyboardOpen) {
+            if (mSliderUnlockScreen && mUpdateMonitor.isKeyguardBypassEnabled() 
+                    && isKeyboardOpen && !mGestureActive) {
                 mCallback.goToUnlockScreen();
                 return;
             }
