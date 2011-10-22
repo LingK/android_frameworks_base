2f28874dc3ce295683b0e279bb9b3505eee702b1
Danesh M <daneshm90@gmail.com>
2011-10-22 16:18:15 -0400

    RingLockScreen : Cleanup/Fixes
    Addressed the issue where with security on :
    
    Screen on -> Unlock screen -> At security screen turn screen off/on
    
    This was caused by : http://review.cyanogenmod.com/#change,8777
    
    In addition to reverting that i did a few more things to serve the original purpose,
    of that patch which was to avoid the lockscreen flash prior to unlocking.
    On some phones this was very noticable.
    
    Change-Id: I92569aa2b0a4d2a791f99f5c9f15713cc2c2cbe9
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/LockScreen.java

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -1384,6 +1384,9 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
 
     /** {@inheritDoc} */
     public void onPause() {
+        if (mUseRingLockscreen) {
+            mRingSelector.setVisibility(View.VISIBLE);
+        }
     }
 
     /** {@inheritDoc} */
@@ -1569,7 +1572,6 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
                 && (Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.LOCKSCREEN_NOW_PLAYING, 1) == 1))
             mNowPlayingToggle = true;
-
         if (visibility == View.VISIBLE
                 && (Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.LOCKSCREEN_ALBUM_ART, 1) == 1))
