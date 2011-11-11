bc4673c03a5c4e25367d26ca3f8ee1fadd1cb3ab
Liquid Smooth <liquid0624@gmail.com>
2011-11-10 22:49:27 -0500

    Revert "Fix slide to unlock"
    This reverts commit 206c1d6eed9441408028974e8b3e352cb4766b26.

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -1534,7 +1534,8 @@ class LockScreen extends LinearLayout implements KeyguardScreen, KeyguardUpdateM
             mKeyboardHidden = newConfig.hardKeyboardHidden;
             final boolean isKeyboardOpen = mKeyboardHidden == Configuration.HARDKEYBOARDHIDDEN_NO;
 
-            if (mSliderUnlockScreen && isKeyboardOpen && !mGestureActive) {
+            if (mSliderUnlockScreen && mUpdateMonitor.isKeyguardBypassEnabled() 
+                    && isKeyboardOpen && !mGestureActive) {
                 mCallback.goToUnlockScreen();
                 return;
             }
